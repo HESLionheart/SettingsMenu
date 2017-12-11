fadcc42c9485f20ae8a226546a73e888c19ce430
Lionheart <hallel1851998@gmail.com>
2017-12-11 18:43:24 +0200

    update

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject canvas;
+
+    public void Awake()
+    {
+        if(canvas==null)
+            canvas= GameObject.Find("MainMenu");
+    }
+
+    public void LoadCanvas()
+    {
+        MenuManager.instance.ActivateCanvas(canvas);
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    public static MenuManager instance;
+
+    [SerializeField]
+    GameObject current;
+    GameObject main;
+    [SerializeField]
+    List<ButtonController> buttons;
+
+    public void Awake()
+    {
+        if (!instance)
+            instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        current = GameObject.Find("MainMenu");
+        main = current;
+    }
+
+    public void ActivateCanvas(GameObject canvas)
+    {
+        current.SetActive(false);
+        current = canvas;
+        current.SetActive(true);
+    }
+}
